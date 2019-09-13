06ea16c65cf706ea8a7d95ab48e48b6ca2017fb9
Damian Szczepanik <damian.publicemail@gmail.com>
2019-09-14 00:30:23 +0200

    Hello world version

---
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
@@ -0,0 +1,34 @@
+package pl.damianszczepanik.jenkins.buildhistorymanager;
+
+import java.io.IOException;
+
+import hudson.Extension;
+import hudson.model.Job;
+import jenkins.model.BuildDiscarder;
+import jenkins.model.BuildDiscarderDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class BuildHistoryManager extends BuildDiscarder {
+
+    public String limit;
+
+    @DataBoundConstructor
+    public BuildHistoryManager(String limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public void perform(Job<?, ?> job) throws IOException, InterruptedException {
+    }
+
+    public String getLimit() {
+        return limit;
+    }
+
+    @Extension
+    public static final class EnhancedOldBuildDiscarderDescriptor extends BuildDiscarderDescriptor {
+        public String getDisplayName() {
+            return "Build History Manager";
+        }
+    }
+}
