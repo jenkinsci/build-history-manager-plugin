9b7c3ab5282ec1dd5d18eb95d3a41ff274a2ed45
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2019-10-03 23:56:15 +0200

    Fix NPE when no action defined (#2)

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/Rule.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/Rule.java
@@ -3,6 +3,7 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.model;
 import java.io.IOException;
 import java.util.List;
 
+import hudson.Util;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Job;
 import hudson.model.Run;
@@ -24,8 +25,8 @@ public class Rule extends AbstractDescribableImpl<Rule> {
 
     @DataBoundConstructor
     public Rule(List<Condition> conditions, List<Action> actions) {
-        this.conditions = conditions;
-        this.actions = actions;
+        this.conditions = Util.fixNull(conditions);
+        this.actions = Util.fixNull(actions);
     }
 
     public List<Condition> getConditions() {
@@ -81,9 +82,9 @@ public class Rule extends AbstractDescribableImpl<Rule> {
             // validate rules for previous build
             run = run.getPreviousBuild();
         } while (
-                // stop when the iteration reach the oldest build
+            // stop when the iteration reach the oldest build
                 run != null &&
-                // stop checking if max number of processed builds is reached
-                matchedTimes <= configuration.getMatchAtMost());
+                        // stop checking if max number of processed builds is reached
+                        matchedTimes <= configuration.getMatchAtMost());
     }
 }
