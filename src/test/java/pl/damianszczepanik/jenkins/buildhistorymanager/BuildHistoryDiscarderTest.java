f70cf50066d08eade6be40357fa2b141fbc37c4f
Damian Szczepanik <damian.publicemail@gmail.com>
2019-10-05 19:27:05 +0200

    Refactor way how jobs and rules are processed   - matchedTimes is calculated properly   - documentation is improved   - additional logs are provided

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryDiscarderTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryDiscarderTest.java
@@ -2,13 +2,11 @@ package pl.damianszczepanik.jenkins.buildhistorymanager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import pl.damianszczepanik.jenkins.buildhistorymanager.model.Rule;
-import pl.damianszczepanik.jenkins.buildhistorymanager.model.RuleBuilder;
 
 /**
  * @author Damian Szczepanik (damianszczepanik@github)
@@ -41,20 +39,5 @@ public class BuildHistoryDiscarderTest {
 
         // then
         assertThat(rules).containsAll(sampleRules);
-    }
-
-    @Test
-    public void perform_IteratesOverRules() throws IOException, InterruptedException {
-
-        // given
-        RuleBuilder.TestRule ruleCounter = new RuleBuilder.TestRule();
-        List<Rule> rules = Arrays.asList(ruleCounter, ruleCounter, ruleCounter);
-        BuildHistoryDiscarder discarder = new BuildHistoryDiscarder(rules);
-
-        // when
-        discarder.perform(null);
-
-        // then
-        assertThat(ruleCounter.times).isEqualTo(rules.size());
     }
 }
--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/RuleBuilder.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/RuleBuilder.java
@@ -1,11 +1,10 @@
 package pl.damianszczepanik.jenkins.buildhistorymanager.model;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 import hudson.model.Descriptor;
-import hudson.model.Job;
+import hudson.model.Run;
 import javax.annotation.Nonnull;
 
 /**
@@ -24,8 +23,9 @@ public class RuleBuilder {
         }
 
         @Override
-        public void perform(Job<?, ?> job) throws IOException, InterruptedException {
+        public boolean validateConditions(Run<?, ?> run) {
             times++;
+            return false;
         }
 
         @Override
