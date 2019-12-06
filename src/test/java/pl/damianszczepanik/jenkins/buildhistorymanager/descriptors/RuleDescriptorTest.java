5be47b6962d21e02acb6ba0e319bf0ea8e7a2113
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2019-12-06 22:51:29 +0100

    Validate matchAtMost parameter for Rule (#26)

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/RuleDescriptorTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/RuleDescriptorTest.java
@@ -3,6 +3,7 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.descriptors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import hudson.model.Descriptor;
+import hudson.util.FormValidation;
 import org.junit.Test;
 
 /**
@@ -21,5 +22,44 @@ public class RuleDescriptorTest {
 
         // then
         assertThat(displayName).isEqualTo("Rule");
+    }
+
+    @Test
+    public void doCheckMatchAtMost_OnValidNumber_ReturnsOK() {
+
+        // given
+        RuleDescriptor descriptor = new RuleDescriptor();
+
+        // when
+        FormValidation formValidation = descriptor.doCheckMatchAtMost(124 + "");
+
+        // then
+        assertThat(formValidation).isEqualTo(FormValidation.ok());
+    }
+
+    @Test
+    public void doCheckMatchAtMost_OnInvalidNumber_ReturnsError() {
+
+        // given
+        RuleDescriptor descriptor = new RuleDescriptor();
+
+        // when
+        FormValidation formValidation = descriptor.doCheckMatchAtMost(124.5 + "");
+
+        // then
+        assertThat(formValidation.kind).isEqualTo(FormValidation.Kind.ERROR);
+    }
+
+    @Test
+    public void doCheckMatchAtMost_OnOutOfRangeNumber_ReturnsError() {
+
+        // given
+        RuleDescriptor descriptor = new RuleDescriptor();
+
+        // when
+        FormValidation formValidation = descriptor.doCheckMatchAtMost(-124 + "");
+
+        // then
+        assertThat(formValidation.kind).isEqualTo(FormValidation.Kind.ERROR);
     }
 }
