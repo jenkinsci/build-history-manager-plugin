e282c3fdbe40b1be803f97808749313223982852
Damian Szczepanik <damian.publicemail@gmail.com>
2019-12-06 22:10:51 +0100

    Validate matchAtMost parameter for Rule

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/RuleDescriptor.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/RuleDescriptor.java
@@ -2,6 +2,9 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.descriptors;
 
 import hudson.Extension;
 import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+import org.kohsuke.stapler.QueryParameter;
+import pl.damianszczepanik.jenkins.buildhistorymanager.Messages;
 import pl.damianszczepanik.jenkins.buildhistorymanager.model.Rule;
 
 /**
@@ -19,5 +22,21 @@ public class RuleDescriptor extends Descriptor<Rule> {
     @Override
     public String getDisplayName() {
         return "Rule";
+    }
+
+    public FormValidation doCheckMatchAtMost(@QueryParameter String matchAtMost) {
+        return isValidInteger(matchAtMost);
+    }
+
+    private static FormValidation isValidInteger(String value) {
+        try {
+            int intValue = Integer.parseInt(value);
+            if (intValue < -1) {
+                return FormValidation.error(Messages.role_notValidMatchAtMost());
+            }
+        } catch (NumberFormatException e) {
+            return FormValidation.error(Messages.notInteger());
+        }
+        return FormValidation.ok();
     }
 }
