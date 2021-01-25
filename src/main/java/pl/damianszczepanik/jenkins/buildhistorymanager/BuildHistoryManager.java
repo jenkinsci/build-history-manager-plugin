ef4758dfb9051cf07c08c99d90e2e9e9cfa135e3
theKBro <K.Broselge@gmx.net>
2021-01-25 07:56:52 +0100

    - add missing synchronization context needed for rule's matchedTimes

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
@@ -40,33 +40,34 @@ public class BuildHistoryManager extends BuildDiscarder {
      */
     @Override
     public void perform(Job<?, ?> job) throws IOException, InterruptedException {
-
-        // reset counters of matched builds
-        for (Rule rule : rules) {
-            rule.initialize();
-        }
-
-        Run<?, ?> run = job.getLastCompletedBuild();
-        // for each completed build...
-        do {
-            LOG.info("Processing build #" + run.getNumber());
-            for (int i = 0; i < rules.size(); i++) {
-                Rule rule = rules.get(i);
-                LOG.info("Processing rule no " + (i + 1));
-                if (rule.validateConditions(run)) {
-                    LOG.info("Processing actions for rule no " + (i + 1));
-                    rule.performActions(run);
-
-                    // if other rules should not be proceed, shift to next build
-                    if (!rule.getContinueAfterMatch()) {
-                        break;
-                    }
-                }
+        synchronized (this) {
+            // reset counters of matched builds
+            for (Rule rule : rules) {
+                rule.initialize();
             }
 
-            // validateConditions rules for previous build - completed in case some previous are still building
-            run = run.getPreviousCompletedBuild();
-            // stop when the iteration reach the oldest build
-        } while (run != null);
+            Run<?, ?> run = job.getLastCompletedBuild();
+            // for each completed build...
+            do {
+                LOG.info("Processing build #" + run.getNumber());
+                for (int i = 0; i < rules.size(); i++) {
+                    Rule rule = rules.get(i);
+                    LOG.info("Processing rule no " + (i + 1));
+                    if (rule.validateConditions(run)) {
+                        LOG.info("Processing actions for rule no " + (i + 1));
+                        rule.performActions(run);
+
+                            // if other rules should not be proceed, shift to next build
+                            if (!rule.getContinueAfterMatch()) {
+                                break;
+                            }
+                        }
+                    }
+
+                // validateConditions rules for previous build - completed in case some previous are still building
+                run = run.getPreviousCompletedBuild();
+                // stop when the iteration reach the oldest build
+            } while (run != null);
+        }
     }
 }
