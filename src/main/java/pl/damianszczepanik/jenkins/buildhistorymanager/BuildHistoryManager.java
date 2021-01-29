842fc16dd4a1b1e6d76118948496e9f1707e764c
theKBro <K.Broselge@gmx.net>
2021-01-29 19:48:26 +0100

    - fix exception if no build was performed at all (#39)

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
@@ -48,7 +48,7 @@ public class BuildHistoryManager extends BuildDiscarder {
 
             Run<?, ?> run = job.getLastCompletedBuild();
             // for each completed build...
-            do {
+            while (run != null) {
                 LOG.info("Processing build #" + run.getNumber());
                 for (int i = 0; i < rules.size(); i++) {
                     Rule rule = rules.get(i);
@@ -57,17 +57,17 @@ public class BuildHistoryManager extends BuildDiscarder {
                         LOG.info("Processing actions for rule no " + (i + 1));
                         rule.performActions(run);
 
-                            // if other rules should not be proceed, shift to next build
-                            if (!rule.getContinueAfterMatch()) {
-                                break;
-                            }
+                        // if other rules should not be proceed, shift to next build
+                        if (!rule.getContinueAfterMatch()) {
+                            break;
                         }
                     }
+                }
 
                 // validateConditions rules for previous build - completed in case some previous are still building
                 run = run.getPreviousCompletedBuild();
                 // stop when the iteration reach the oldest build
-            } while (run != null);
+            }
         }
     }
 }
