527c95afa58aaa7054fa55a13d7337daf6a09629
theKBro <K.Broselge@gmail.com>
2021-01-30 19:34:45 +0100

    move synchronize to method signature

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/BuildHistoryManager.java
@@ -39,35 +39,33 @@ public class BuildHistoryManager extends BuildDiscarder {
      * @see Job#logRotate()
      */
     @Override
-    public void perform(Job<?, ?> job) throws IOException, InterruptedException {
-        synchronized (this) {
-            // reset counters of matched builds
-            for (Rule rule : rules) {
-                rule.initialize();
-            }
+    public synchronized void perform(Job<?, ?> job) throws IOException, InterruptedException {
+        // reset counters of matched builds
+        for (Rule rule : rules) {
+            rule.initialize();
+        }
 
-            Run<?, ?> run = job.getLastCompletedBuild();
-            // for each completed build...
-            while (run != null) {
-                LOG.info("Processing build #" + run.getNumber());
-                for (int i = 0; i < rules.size(); i++) {
-                    Rule rule = rules.get(i);
-                    LOG.info("Processing rule no " + (i + 1));
-                    if (rule.validateConditions(run)) {
-                        LOG.info("Processing actions for rule no " + (i + 1));
-                        rule.performActions(run);
+        Run<?, ?> run = job.getLastCompletedBuild();
+        // for each completed build...
+        while (run != null) {
+            LOG.info("Processing build #" + run.getNumber());
+            for (int i = 0; i < rules.size(); i++) {
+                Rule rule = rules.get(i);
+                LOG.info("Processing rule no " + (i + 1));
+                if (rule.validateConditions(run)) {
+                    LOG.info("Processing actions for rule no " + (i + 1));
+                    rule.performActions(run);
 
-                        // if other rules should not be proceed, shift to next build
-                        if (!rule.getContinueAfterMatch()) {
-                            break;
-                        }
+                    // if other rules should not be proceed, shift to next build
+                    if (!rule.getContinueAfterMatch()) {
+                        break;
                     }
                 }
-
-                // validateConditions rules for previous build - completed in case some previous are still building
-                run = run.getPreviousCompletedBuild();
-                // stop when the iteration reach the oldest build
             }
+
+            // validateConditions rules for previous build - completed in case some previous are still building
+            run = run.getPreviousCompletedBuild();
+            // stop when the iteration reach the oldest build
         }
     }
 }
