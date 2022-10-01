7d621fb1aae9d5de3a71776d7ac3a0eb00bf6844
Damian Szczepanik <damian.publicemail@gmail.com>
2022-10-01 21:43:40 +0200

    Support for build cause condition
    Issue #60

---
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/CauseCondition.java
@@ -0,0 +1,43 @@
+package pl.damianszczepanik.jenkins.buildhistorymanager.model.conditions;
+
+import hudson.model.Cause;
+import hudson.model.Run;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import pl.damianszczepanik.jenkins.buildhistorymanager.model.RuleConfiguration;
+
+/**
+ * Matches builds for which class name of {@link Cause} matches given class.
+ *
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+public class CauseCondition extends Condition {
+
+    private String causeClass;
+
+    @DataBoundConstructor
+    public CauseCondition() {
+        // Jenkins stapler requires to have public constructor with @DataBoundConstructor
+    }
+
+    public String getCauseClass() {
+        return causeClass;
+    }
+
+    @DataBoundSetter
+    public void setCauseClass(String causeClass) {
+        this.causeClass = causeClass;
+    }
+
+    @Override
+    public boolean matches(Run<?, ?> run, RuleConfiguration configuration) {
+        for (Cause cause : run.getCauses()) {
+            // use contains() method to avoid problem with class name
+            // for causes which are often inner class and name contains $ character
+            if (cause.toString().contains(causeClass)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
