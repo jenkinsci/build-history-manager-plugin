32a06f9e7ca38275ae255ffc0ecb1b971ab07283
Nurgul Amat <namat@partner.ciena.com>
2023-07-31 09:30:01 -0400

    Remove '@Generated' annotation
    We will add tests for getters/setters instead of excluding them from codecoverage

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsWithPatternActionDescriptor.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsWithPatternActionDescriptor.java
@@ -9,7 +9,6 @@ import hudson.Extension;
 import hudson.model.Descriptor;
 import pl.damianszczepanik.jenkins.buildhistorymanager.model.actions.Action;
 import pl.damianszczepanik.jenkins.buildhistorymanager.model.actions.DeleteArtifactsWithPatternAction;
-import javax.annotation.Generated;
 
 /**
  * Descriptor implementation needed to render UI for {@link DeleteArtifactsWithPatternAction}.
@@ -22,24 +21,20 @@ public class DeleteArtifactsWithPatternActionDescriptor extends Descriptor<Actio
     private String include;
     private String exclude;
 
-    @Generated(value = "JaCoCo") // Exclude this method from code coverage
     public String getInclude() {
         return include;
     }
 
     @DataBoundSetter
-    @Generated(value = "JaCoCo") // Exclude this method from code coverage
     public void setInclude(String include) {
         this.include = include;
     }
 
-    @Generated(value = "JaCoCo") // Exclude this method from code coverage
     public String getExclude() {
         return exclude;
     }
 
     @DataBoundSetter
-    @Generated(value = "JaCoCo") // Exclude this method from code coverage
     public void setExclude(String exclude) {
         this.exclude = exclude;
     }
