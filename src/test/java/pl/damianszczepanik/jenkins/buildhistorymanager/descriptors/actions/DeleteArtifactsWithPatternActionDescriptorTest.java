32b604c2ca2d525a6c3027e863b867a6f5c27394
Nurgul Amat <namat@partner.ciena.com>
2023-07-31 14:16:07 -0400

    Add unit tests for getInclude and getExclude methods for code coverage

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsWithPatternActionDescriptorTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsWithPatternActionDescriptorTest.java
@@ -2,16 +2,28 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.descriptors.actions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import hudson.model.AbstractDescribableImpl;
-import hudson.model.Descriptor;
+import java.io.IOException;
+
 import org.jenkinsci.plugins.structs.SymbolLookup;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
+
 public class DeleteArtifactsWithPatternActionDescriptorTest {
+    private DeleteArtifactsWithPatternActionDescriptor descriptor;
+
     @Rule
     public JenkinsRule j = new JenkinsRule();
+
+    @Before
+    public void setUp() {
+        descriptor = new DeleteArtifactsWithPatternActionDescriptor();
+    }
 
     @Test
     public void getDisplayName_ReturnsDescriptorName() {
@@ -26,4 +38,30 @@ public class DeleteArtifactsWithPatternActionDescriptorTest {
         assertThat(displayName).isEqualTo("Delete artifacts with pattern");
     }
     
+    @Test // testing getInclude method for code coverage
+    public void testGetInclude() throws IOException, InterruptedException {
+
+        // given
+        String include = "includePattern";
+        descriptor.setInclude(include);
+
+        // when
+        String result = descriptor.getInclude();
+
+        // then
+        Assert.assertEquals(include, result);
+    }
+
+    @Test
+    public void testGetExclude() {
+        // Given
+        String exclude = "excludePattern";
+        descriptor.setExclude(exclude);
+
+        // When
+        String result = descriptor.getExclude();
+
+        // Then
+        Assert.assertEquals(exclude, result);
+    }
 }
