9f33d38f5f900f3072198a5649846e7e9feb2f3b
Nurgul Amat <namat@partner.ciena.com>
2023-09-14 08:33:25 -0400

    Rename as maxBuildPosition

--- src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/BuildPositionCondition.java
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/BuildPositionCondition.java
@@ -8,25 +8,25 @@ import pl.damianszczepanik.jenkins.buildhistorymanager.model.RuleConfiguration;
 
 public class BuildPositionCondition extends Condition {
 
-    private int buildPosition;
+    private int maxBuildPosition;
 
     @DataBoundConstructor
     public BuildPositionCondition() {
       // Jenkins stapler requires to have public constructor with @DataBoundConstructor  
     }
 
-    public int getBuildPosition() {
-        return buildPosition;
+    public int getMaxBuildPosition() {
+        return maxBuildPosition;
     }
 
     @DataBoundSetter
-    public void setBuildPosition(int buildPosition) {
-        this.buildPosition = buildPosition;
+    public void setMaxBuildPosition(int maxBuildPosition) {
+        this.maxBuildPosition = maxBuildPosition;
     }
 
     @Override
     public boolean matches(Run<?, ?> run, RuleConfiguration configuration) {
-        return false;
+        // return false;
         // return run.getNumber() >= minBuildNumber && run.getNumber() <= maxBuildNumber;
     } 
 }
