0be8337b06a4a49923168098f065ef689c6f4d30
Nurgul Amat <namat@partner.ciena.com>
2023-10-05 11:57:15 -0400

    Refacotr: Removed redundent code and organized the import statements

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsMatchingPatternsActionDescriptorTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/actions/DeleteArtifactsMatchingPatternsActionDescriptorTest.java
@@ -2,25 +2,17 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.descriptors.actions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hudson.model.AbstractDescribableImpl;
+import hudson.model.Descriptor;
 import org.jenkinsci.plugins.structs.SymbolLookup;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import hudson.model.AbstractDescribableImpl;
-import hudson.model.Descriptor;
-
 public class DeleteArtifactsMatchingPatternsActionDescriptorTest {
-    private DeleteArtifactsMatchingPatternsActionDescriptor descriptor;
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
-
-    @Before
-    public void setUp() {
-        descriptor = new DeleteArtifactsMatchingPatternsActionDescriptor();
-    }
 
     @Test
     public void getDisplayName_ReturnsDescriptorName() {
