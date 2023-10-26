870c77abd1eb3526c50840517c0cb122bafb431b
Nurgul Amat <namat@partner.ciena.com>
2023-10-25 21:08:34 -0400

    Removed redundant codes and comments

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/BuildPositionConditionIT.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/BuildPositionConditionIT.java
@@ -25,50 +25,32 @@ public class BuildPositionConditionIT {
     public JenkinsRule jenkinsRule = new JenkinsRule();
  
     private WorkflowJob createPipelineJobFromScriptFile(String fileName) throws IOException {
-        // Get the path to the test file in the test resources directory
         Path pipelineScriptPath = Paths.get("src/test/resources", fileName);
-
-        // Read the content of the test file into a string
         String pipelineScript = new String(Files.readAllBytes(pipelineScriptPath));
 
-        // Create the CpsFlowDefinition with the loaded script
         CpsFlowDefinition cpsFlowDefinition = new CpsFlowDefinition(pipelineScript, true);
-
-        // Create a Jenkins pipeline job
         WorkflowJob pipelineJob = jenkinsRule.jenkins.createProject(WorkflowJob.class, fileName);
 
-        // Set the pipeline script for the job
         pipelineJob.setDefinition(cpsFlowDefinition);
         return pipelineJob;
     }
 
-    private WorkflowRun executeAndWaitForSuccessfulRun(WorkflowJob pipelineJob) throws InterruptedException, ExecutionException, IOException {
+    private WorkflowRun executeAndWaitForSuccessfulRun(WorkflowJob pipelineJob) throws InterruptedException, ExecutionException {
         WorkflowRun run = pipelineJob.scheduleBuild2(0).get();
-
-        // Wait for the build to complete
         jenkinsRule.waitForCompletion(run);
-
-        // Get the build result
-        Result result = run.getResult();
-
-        // Assertion for the build success
-        Assert.assertEquals(run.getLog(), Result.SUCCESS, result);
         return run;
     }
 
     @Test
     public void queueMultipleRuns() throws Exception {
         WorkflowJob pipelineJob = createPipelineJobFromScriptFile("buildPositionCondition_KeepLastTenBuildsTest.jf");
-        //Create an ArrayList to store the runs
         List<WorkflowRun> arrayList = new ArrayList<>();
-        for (int i = 1; i <= 15; i++ ) {    
-            // Execute the job and wait for a successful run
+        for (int i = 1; i <= 15; i++ ) {
             WorkflowRun run = executeAndWaitForSuccessfulRun(pipelineJob);
             arrayList.add(run);
         }
 
         RunList<WorkflowRun> runList = pipelineJob.getBuilds();
-
         int numOfRuns = 0;
         for (WorkflowRun run : runList) {
             numOfRuns++;
