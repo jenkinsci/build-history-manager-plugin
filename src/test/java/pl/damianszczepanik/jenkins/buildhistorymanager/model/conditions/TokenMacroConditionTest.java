743a5e16139b160baf5723049bb342e7f5f8b260
Damian Szczepanik <damian.publicemail@gmail.com>
2024-04-01 11:52:10 +0200

    Uses invalid token syntax for testing over mocking method

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/TokenMacroConditionTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/conditions/TokenMacroConditionTest.java
@@ -2,21 +2,14 @@ package pl.damianszczepanik.jenkins.buildhistorymanager.model.conditions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 import java.io.File;
-import java.io.IOException;
 
-import hudson.FilePath;
 import hudson.model.Run;
-import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
-import org.jenkinsci.plugins.tokenmacro.TokenMacro;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
@@ -89,24 +82,14 @@ public class TokenMacroConditionTest {
     }
 
     @Test
-    @PrepareForTest({TokenMacro.class, FilePath.class})
-    public void matches_OnInvalidTemplate_ReturnsFalse() {
+    public void matches_OnInvalidTemplateSyntax_ReturnsFalse() {
 
         // given
-        String template = "myTemplate";
+        String template = "myTemplate$$";
         String value = "myValue";
         TokenMacroCondition condition = new TokenMacroCondition(template, value);
 
         Run run = mockRun();
-        mockStatic(TokenMacro.class);
-        mockStatic(FilePath.class);
-
-        try {
-            when(TokenMacro.expandAll(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any()))
-                    .thenThrow(new MacroEvaluationException("ups!"));
-        } catch (MacroEvaluationException | IOException | InterruptedException e) {
-            throw new IllegalArgumentException(e);
-        }
 
         // when
         boolean matched = condition.matches(run, null);
