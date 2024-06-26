3c860bda48fb8d2d365ae682267e82d96870a545
Damian Szczepanik <damianszczepanik@users.noreply.github.com>
2024-06-26 19:53:47 +0200

    Adds condition based on build description (#129)
    Request #128

---
+++ src/main/java/pl/damianszczepanik/jenkins/buildhistorymanager/descriptors/conditions/BuildDescriptionDescriptor.java
@@ -0,0 +1,39 @@
+package pl.damianszczepanik.jenkins.buildhistorymanager.descriptors.conditions;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.util.ListBoxModel;
+import org.jenkinsci.Symbol;
+import pl.damianszczepanik.jenkins.buildhistorymanager.Messages;
+import pl.damianszczepanik.jenkins.buildhistorymanager.model.conditions.BuildDescriptionCondition;
+import pl.damianszczepanik.jenkins.buildhistorymanager.model.conditions.BuildDescriptionCondition.MatchingMethodType;
+import pl.damianszczepanik.jenkins.buildhistorymanager.model.conditions.Condition;
+
+/**
+ * Descriptor implementation needed to render UI for {@link BuildDescriptionCondition}.
+ *
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+@Extension
+@Symbol("BuildDescription")
+public class BuildDescriptionDescriptor extends Descriptor<Condition> {
+
+    public BuildDescriptionDescriptor() {
+        super(BuildDescriptionCondition.class);
+    }
+
+    // names must refer to the field name
+    public ListBoxModel doFillMatchingMethodItems() {
+        return new ListBoxModel(
+                // default option should be listed first
+                new ListBoxModel.Option(Messages.matchingMethodType_EQUALS(), MatchingMethodType.EQUALS.name()),
+                new ListBoxModel.Option(Messages.matchingMethodType_CONTAINS(), MatchingMethodType.CONTAINS.name()),
+                new ListBoxModel.Option(Messages.matchingMethodType_MATCHES(), MatchingMethodType.MATCHES.name())
+        );
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Build description";
+    }
+}
