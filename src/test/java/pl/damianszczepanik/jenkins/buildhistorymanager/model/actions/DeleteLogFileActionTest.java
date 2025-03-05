3a0e17558b9a9773be7c4e66af9e5df964ee398d
theKBro <K.Broselge@gmx.net>
2025-03-05 22:51:04 +0100

    add testcases

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/actions/DeleteLogFileActionTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/actions/DeleteLogFileActionTest.java
@@ -1,8 +1,10 @@
 package pl.damianszczepanik.jenkins.buildhistorymanager.model.actions;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+import static org.junit.Assert.assertThrows;
 import pl.damianszczepanik.jenkins.buildhistorymanager.utils.RunStub;
 
 /**
@@ -22,5 +24,50 @@ public class DeleteLogFileActionTest {
 
         // then
         run.assertLogFileWasNotDeleted();
+    }
+
+    @Test
+    public void perform_OnGetLogFileThrowsUnsupportedOperationException_CatchesExceptionAndDoesNothing() throws IOException, InterruptedException {
+
+        // given
+        Action action = new DeleteLogFileAction();
+        RunStub run = new RunStub() {
+            @Override
+            public File getLogFile() {
+                throw new UnsupportedOperationException("Operation not supported");
+            }
+        };
+
+        // when
+        action.perform(run);
+
+        // then
+        run.assertLogFileWasNotDeleted();
+    }
+
+    @Test
+    public void perform_OnExistingLogFile_DeletesLogFile() throws IOException, InterruptedException {
+
+        // given
+        Action action = new DeleteLogFileAction();
+        RunStub run = new RunStub() {
+            private final File logFile = File.createTempFile("log", ".txt");
+
+            @Override
+            public File getLogFile() {
+                return logFile;
+            }
+
+            @Override
+            public void deleteLogFile() {
+                logFile.delete();
+            }
+        };
+
+        // when
+        action.perform(run);
+
+        // then
+        run.assertLogFileWasDeleted();
     }
 }
