89a3b611d20b049cd1686fba5a51b90f74d444f4
theKBro <K.Broselge@gmx.net>
2025-03-05 22:51:04 +0100

    add testcases

--- src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/actions/DeleteLogFileActionTest.java
+++ src/test/java/pl/damianszczepanik/jenkins/buildhistorymanager/model/actions/DeleteLogFileActionTest.java
@@ -1,5 +1,6 @@
 package pl.damianszczepanik.jenkins.buildhistorymanager.model.actions;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
@@ -11,16 +12,50 @@ import pl.damianszczepanik.jenkins.buildhistorymanager.utils.RunStub;
 class DeleteLogFileActionTest {
 
     @Test
-    void perform_OnMissingLogFile_SkipDeletion() throws IOException, InterruptedException {
+    void perform_OnExistingLogFile_DeletesLogFile() throws IOException, InterruptedException {
 
         // given
         Action action = new DeleteLogFileAction();
-        RunStub run = new RunStub();
+        RunStub run = new RunStub(RunStub.LogFileAvailability.PRESENT);
 
         // when
         action.perform(run);
 
         // then
-        run.assertLogFileWasNotDeleted();
+        run.assertLogFileIsNotAvailable();
     }
+    
+    @Test
+    void perform_OnMissingLogFile_SkipDeletion() throws IOException, InterruptedException {
+
+        // given
+        Action action = new DeleteLogFileAction();
+        RunStub run = new RunStub(RunStub.LogFileAvailability.ABSENT);
+
+        // when
+        action.perform(run);
+
+        // then
+        run.assertLogFileIsNotAvailable();
+    }
+
+    @Test
+    void perform_OnGetLogFileThrowsUnsupportedOperationException_CatchesExceptionAndDoesNothing() throws IOException, InterruptedException {
+
+        // given
+        Action action = new DeleteLogFileAction();
+        RunStub run = new RunStub(RunStub.LogFileAvailability.PRESENT) {
+            @Override
+            public File getLogFile() {
+                throw new UnsupportedOperationException("Operation not supported");
+            }
+        };
+
+        // when
+        action.perform(run);
+
+        // then
+        run.assertLogFileIsAvailable();
+    }
+    
 }
